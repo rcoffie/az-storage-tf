6b55006be70790803a1f7a8484208878a36978c7
Robert Coffie <rcoffie22@yahoo.com>
2024-10-25 14:04:04 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  # resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "storage-rg"
+  location = "eastus"
+}
+
+# Create storage account
+resource "azurerm_storage_account" "storage_account" {
+  name                = "storemenow01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  account_tier        = "Standard"
+  account_kind        = "StorageV2"
+
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
---
+++ todo.md
@@ -0,0 +1,19 @@
+# Azure Storage
+
+* Design the project
+* Create a storage account with Terraform
+* Storage Resource group name -> storage
+* Storage Account name storemenow01
+* Performance standard
+* Redundant storage -> Geo redundant storage
+* [azure storage sas](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/storage_account_sas)
+* [azure storage](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account)
+* Document the access key process in the documentation
+* Try and explain which type of SAS you are using in the documentation
+  * services sas
+  * account sas
+  * user sas
+* Manage Storage Access Keys
+* Azure AD authentication for a storage Account
+* Authenticate with the account access key
+* Azure Storage Encryption
